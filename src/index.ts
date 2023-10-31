d22d90f9043f6e26dc64a9addb523e1bac98bdb8
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-10-31 00:03:52 -0300

    🚀: Deploy da aplicação dentro da Azure

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,10 @@ MongoDataSource.initialize()
 
 const app = express();
 
-app.listen(5000, ()=>{
-    console.log("App is running on port 5000")
+let port = process.env.PORT_PRODUCTION || 5000
+
+app.listen(port, ()=>{
+    console.log(`App is running on port ${port}`)
 })
 
 app.use(cors())
