310cb6cb2101f0b81ed3623f7d161610549b6f2e
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-11-11 17:07:38 -0300

    ♻️: Formatação do código

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes';
+import express from "express";
+import cors from "cors";
+import router from "./routes";
 import {DataBaseSource} from "./config/database";
-import {MongoDataSource} from './config/mongoConfig';
-import './config/firebase'
+import {MongoDataSource} from "./config/mongoConfig";
+import "./config/firebase";
 DataBaseSource.initialize()
-    .then(() => {
-        console.log("Banco inicializado com sucesso!")
-    })
-    .catch((err) => {
-        console.error("Erro durante a inicialização do banco: ", err)
-    })
+  .then(() => {
+    console.log("Banco inicializado com sucesso!");
+  })
+  .catch((err) => {
+    console.error("Erro durante a inicialização do banco: ", err);
+  });
 
-console.log('Conectando ao MongoDB...');
+console.log("Conectando ao MongoDB...");
 MongoDataSource.initialize()
-    .then(() => {
-        console.log("MongoDB inicializado com sucesso!")
-    })
-    .catch((err) => {
-        console.error("Erro durante a inicialização do MongoDB: ", err)
-    })
+  .then(() => {
+    console.log("MongoDB inicializado com sucesso!");
+  })
+  .catch((err) => {
+    console.error("Erro durante a inicialização do MongoDB: ", err);
+  });
 
 const app = express();
 
-let port = process.env.PORT_PRODUCTION || 5000
+let port = process.env.PORT_PRODUCTION || 5000;
 
-app.listen(port, ()=>{
-    console.log(`App is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
 
-app.use(cors())
-app.use(express.json())
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 export default app;
